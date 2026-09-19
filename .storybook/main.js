7d34265d8911b3d6957d2cd7a23c73c56feed35a
macanderson <mac@oxagen.sh>
2026-09-19 10:52:57 -0700

    save

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,7 @@ import { mockupPlugin } from "./mockup-plugin.mjs";
 export default {
   framework: "@storybook/html-vite",
   stories: ["../mockups/stories/**/*.mdx", "../mockups/stories/**/*.stories.js"],
-  addons: ["@storybook/addon-docs"],
+  addons: ["@storybook/addon-docs", "@storybook/addon-vitest"],
   core: { disableTelemetry: true },
   async viteFinal(config) {
     config.plugins = [...(config.plugins || []), mockupPlugin()];
